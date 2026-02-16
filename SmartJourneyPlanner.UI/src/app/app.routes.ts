135cf7cd8693502b2f536308402cb073de21aaf3
ththsarani dinuri <dinuriththsarani@gmail.com>
2026-02-17 00:42:25 +0530

    Add to login and signup feature

--- SmartJourneyPlanner.UI/src/app/app.routes.ts
+++ SmartJourneyPlanner.UI/src/app/app.routes.ts
@@ -1,7 +1,8 @@
 import { Routes } from '@angular/router';
 import { BudgetDashboard } from './budget-dashboard/budget-dashboard'; // check path
 import { ExpenseForm } from './expense-form/expense-form'; // check path
-
+import { LoginComponent } from './login/login';
+import { SignupComponent } from './signup/signup'; 
 export const routes: Routes = [
   // 1. The Default Home Route (Dashboard)
   { path: '', component: BudgetDashboard },
@@ -11,5 +12,9 @@ export const routes: Routes = [
 
   // 3. (Optional) Explicit Dashboard Route
   // If you add this line, then navigate(['/dashboard']) WILL work.
-  { path: 'dashboard', component: BudgetDashboard }
+  { path: 'dashboard', component: BudgetDashboard },
+
+   { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent }, // path to signup
+  { path: '', redirectTo: '/login', pathMatch: 'full' } // firsly show login
 ];
