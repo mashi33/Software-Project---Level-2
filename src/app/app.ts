2f21127702d8655d34245000a61d195f400e4d89
ththsarani dinuri <dinuriththsarani@gmail.com>
2026-03-20 20:45:31 +0530

    save login and signup base changes before switching

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from './login/login'; 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, LoginComponent], 
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css' 
 })
 export class App {
-  protected readonly title = signal('smart-journey-ui');
+  title = 'smart-journey-ui';
 }
