7e10db834dfa5bccde1b2d737371b525ee38135f
mashi33 <mycreate333@gmail.com>
2026-04-20 21:13:11 +0530

    provider dashboard part

---
+++ SmartJourneyPlanner.API/Controllers/ProviderDashboardController.cs
@@ -0,0 +1,48 @@
+using Microsoft.AspNetCore.Mvc;
+using SmartJourney.Api.Services;
+using System.Threading.Tasks;
+
+namespace SmartJourney.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProviderDashboardController : ControllerBase
+    {
+        private readonly ProviderDashboardService _dashboardService;
+
+        public ProviderDashboardController(ProviderDashboardService dashboardService)
+        {
+            _dashboardService = dashboardService;
+        }
+
+        [HttpGet("stats")]
+        public async Task<IActionResult> GetStats() => Ok(await _dashboardService.GetDashboardStats());
+
+        [HttpGet("vehicles")]
+        public async Task<IActionResult> GetVehicles() => Ok(await _dashboardService.GetAllVehicles());
+
+        [HttpDelete("vehicles/{id}")]
+        public async Task<IActionResult> DeleteVehicle(string id)
+        {
+            await _dashboardService.DeleteVehicle(id);
+            return NoContent();
+        }
+
+        [HttpPut("vehicles/{id}/availability")]
+        public async Task<IActionResult> UpdateAvailability(string id, [FromBody] bool available)
+        {
+            await _dashboardService.UpdateVehicleAvailability(id, available);
+            return Ok();
+        }
+
+        [HttpGet("bookings")]
+        public async Task<IActionResult> GetBookings() => Ok(await _dashboardService.GetAllBookings());
+
+        [HttpDelete("bookings/{id}")]
+        public async Task<IActionResult> DeleteBooking(string id)
+        {
+            await _dashboardService.DeleteBooking(id);
+            return NoContent();
+        }
+    }
+}
