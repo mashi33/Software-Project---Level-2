7f928c5ac15ffa2df01d45ed99faef9daaf62d04
mashi33 <mycreate333@gmail.com>
2026-04-23 17:47:02 +0530

    change ProviderDashboardController.cs and delete Vehicle.cs

--- SmartJourneyPlanner.API/Services/ProviderDashboardService.cs
+++ SmartJourneyPlanner.API/Services/ProviderDashboardService.cs
@@ -1,5 +1,5 @@
 using MongoDB.Driver;
-using SmartJourneyPlanner.API.Models;
+using SmartJourneyPlanner.Models; // This must match your model's namespace exactly
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -7,14 +7,14 @@ namespace SmartJourney.Api.Services
 {
     public class ProviderDashboardService
     {
-        private readonly IMongoCollection<Vehicle> _vehicleCollection;
-        private readonly IMongoCollection<Booking> _bookingCollection;
+        private readonly IMongoCollection<TransportVehicle> _vehicleCollection;
+        private readonly IMongoCollection<TransportBooking> _bookingCollection;
 
         public ProviderDashboardService(IMongoClient mongoClient)
         {
             var database = mongoClient.GetDatabase("travelPlanner");
-            _vehicleCollection = database.GetCollection<Vehicle>("Vehicles");
-            _bookingCollection = database.GetCollection<Booking>("Bookings");
+            _vehicleCollection = database.GetCollection<TransportVehicle>("Vehicles");
+            _bookingCollection = database.GetCollection<TransportBooking>("Bookings");
         }
 
         // --- Dashboard Statistics ---
@@ -26,21 +26,23 @@ namespace SmartJourney.Api.Services
         }
 
         // --- Vehicle Operations ---
-        public async Task<List<Vehicle>> GetAllVehicles() 
+        public async Task<List<TransportVehicle>> GetAllVehicles() 
             => await _vehicleCollection.Find(_ => true).ToListAsync();
 
         public async Task DeleteVehicle(string vehicleId) 
             => await _vehicleCollection.DeleteOneAsync(vehicle => vehicle.Id == vehicleId);
 
-        public async Task UpdateVehicleAvailability(string vehicleId, bool isAvailable)
+        public async Task UpdateVehicleAvailability(string vehicleId, string newStatus)
         {
-            var filter = Builders<Vehicle>.Filter.Eq(vehicle => vehicle.Id, vehicleId);
-            var update = Builders<Vehicle>.Update.Set(vehicle => vehicle.Available, isAvailable);
+            // Note: Your model did not have an 'Available' property. 
+            // I updated this to use your existing 'Status' property instead.
+            var filter = Builders<TransportVehicle>.Filter.Eq(vehicle => vehicle.Id, vehicleId);
+            var update = Builders<TransportVehicle>.Update.Set(vehicle => vehicle.Status, newStatus);
             await _vehicleCollection.UpdateOneAsync(filter, update);
         }
 
         // --- Booking Operations ---
-        public async Task<List<Booking>> GetAllBookings() 
+        public async Task<List<TransportBooking>> GetAllBookings() 
             => await _bookingCollection.Find(_ => true).ToListAsync();
 
         public async Task DeleteBooking(string bookingId) 
