d0105155d19d5639bc020a911a5500cbc823aaa2
Sasni Hansani <sasi.hansani20@gmail.com>
2026-04-23 11:38:21 +0000

    fix the file vehicle.cs

--- SmartJourneyPlanner.API/Models/Vehicle.cs
+++ SmartJourneyPlanner.API/Models/Vehicle.cs
@@ -1,33 +1,34 @@
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 
-namespace SmartJourney.Api.Models
+namespace SmartJourneyPlanner.API.Models
+
 {
-  public class Vehicle
-  {
-    [BsonId]
-    [BsonRepresentation(BsonType.ObjectId)]
-    public string? Id { get; set; }
+    public class Vehicle
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
 
-    public string Name { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        
+        public string Type { get; set; } = string.Empty;
+        
+        public bool Available { get; set; }
+    }
 
-    public string Type { get; set; } = string.Empty;
+    public class Booking
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
 
-    public bool Available { get; set; }
-  }
-
-  public class Booking
-  {
-    [BsonId]
-    [BsonRepresentation(BsonType.ObjectId)]
-    public string? Id { get; set; }
-
-    public string CustomerName { get; set; } = string.Empty;
-
-    public string VehicleId { get; set; } = string.Empty;
-
-    public string Status { get; set; } = "Pending";
-
-    public string BookingDate { get; set; } = string.Empty;
-  }
+        public string CustomerName { get; set; } = string.Empty;
+        
+        public string VehicleId { get; set; } = string.Empty;
+        
+        public string Status { get; set; } = "Pending";
+        
+        public string BookingDate { get; set; } = string.Empty;
+    }
 }
