fd1916539ea0d6680a26862522b7b72fbe2b5eed
ththsarani dinuri <dinuriththsarani@gmail.com>
2026-04-24 06:29:11 +0530

    Implement role-based routing and login redirect logic

--- SmartJourneyPlanner.UI/src/app/login/login.ts
+++ SmartJourneyPlanner.UI/src/app/login/login.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AuthService } from '../services/auth.service';
-import { Router, RouterLink } from '@angular/router'; // Router යන්න මෙහි අනිවාර්යයෙන්ම තිබිය යුතුයි
+import { Router, RouterLink } from '@angular/router'; 
 
 @Component({
   selector: 'app-login',
@@ -11,7 +11,7 @@ import { Router, RouterLink } from '@angular/router'; // Router යන්න ම
   styleUrl: './login.css',
 })
 export class LoginComponent {
-  // මෙහිදී අපි දත්ත ලබා ගැනීමට variable දෙකක් හදමු
+  //  variable to get data from form
   loginData = {
     email: '',
     password: ''
@@ -22,11 +22,26 @@ constructor(private authService: AuthService , private router: Router) { }
   onLogin() {
     this.authService.login(this.loginData).subscribe({
       next: (response) => {
+        // 1. Save token and userType in localStorage for later use
+        localStorage.setItem('token', response.token);
+        localStorage.setItem('userType', response.userType);
+
         console.log('Login Success!', response);
         alert('Login Successful!');
-        
-        this.router.navigate(['/signup']);
-        
+
+        // 2. UserType 
+        if (response.userType === 'TransportProvider') {
+          this.router.navigate(['/provider-dashboard']);
+        } 
+        else if (response.userType === 'Traveller') {
+          this.router.navigate(['/traveller-dashboard']);
+        } 
+        else if (response.userType === 'Admin') {
+          this.router.navigate(['/admin-dashboard']);
+        } 
+        else {
+          this.router.navigate(['/']); 
+        }
       },
       error: (err) => {
         console.error('Login Failed', err);
