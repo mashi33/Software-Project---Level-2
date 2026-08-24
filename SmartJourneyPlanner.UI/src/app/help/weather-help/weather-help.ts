12090004e654d9aeb1c04c38bc20d824fb51d852
mashi33 <mycreate333@gmail.com>
2026-08-24 11:12:23 +0530

    make help pages

---
+++ SmartJourneyPlanner.UI/src/app/help/weather-help/weather-help.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-weather-help',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './weather-help.html',
+  styleUrls: ['./weather-help.css']
+})
+export class WeatherHelpComponent {
+  
+}
